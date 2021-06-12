13c3cde8a169e78fc8365941af510b4a63de42ce
Majored <hello@majored.pw>
2021-06-12 23:34:17 +0100

    Addition of comments to public APIWrapper functions

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ use reqwest::header::HeaderMap;
 use serde::Deserialize;
 use serde::de::DeserializeOwned;
 
-const BASE_URL: &str = "https://api.mc-market.org/v1";
+pub const BASE_URL: &str = "https://api.mc-market.org/v1";
 
 pub enum APIToken {
     Private(String),
@@ -61,6 +61,20 @@ pub struct APIWrapper {
 }
 
 impl APIWrapper {
+    /// Build an API wrapper instance from a raw API token.
+    /// 
+    /// # Note
+    /// During the build process, we make a request to the `health` endpoint which we expect to always succeed under
+    /// nominal conditions. If the request does fail, we expect subsequent requests to other endpoint to also fail so
+    /// we conclude that a build failure has occured.
+    /// 
+    /// # Example
+    /// ```
+    /// let token = APIToken::Private(String::from("y6xWrGkAzh8Gp4qBWFMG7tDyB+zB+Lub"));
+    /// let wrapper = APIWrapper::build(token).await.unwrap();
+    /// 
+    /// println!("Successfully connected to the API.");
+    /// ```
     pub async fn build(token: APIToken) -> Result<APIWrapper, APIError> {
         let mut default_headers = HeaderMap::new();
         default_headers.insert("Authorization", token.as_header().parse().unwrap());
@@ -113,6 +127,13 @@ impl APIWrapper {
         }
     }
 
+    /// Schedule a plain request which we expect to always succeed under nominal conditions.
+    /// 
+    /// # Example
+    /// ```
+    /// wrapper.health().await?;
+    /// println!("Received a successful response from the API.");
+    /// ```
     pub async fn health(&self) -> Result<(), APIError> {
         let data: String = self.get("/health").await?;
 
@@ -123,22 +144,14 @@ impl APIWrapper {
         Ok(())
     }
 
-    pub async fn fetch_metrics(&self) -> Result<MetricsSnapshot, APIError> {
-        self.get("/metrics").await
-    }
-
-    pub async fn fetch_member(&self, member_id: u64) -> Result<Member, APIError> {
-        self.get(&format!("/members/{}", member_id)[..]).await
-    }
-
     /// Schedule a plain request and measure how long the API took to respond.
     /// 
-    /// Note:
+    /// # Note
     /// This duration may not be representative of the raw request latency due to the fact that requests may be stalled
     /// locally within this wrapper to ensure compliance with rate limiting rules. Whilst this is a trade-off, it can
     /// be argued that the returned duration will be more representative of the true latencies experienced.
     /// 
-    /// Example:
+    /// # Example
     /// ```
     /// println!("Took {}ms for the API to respond.", wrapper.ping().await?.as_millis());
     /// ```
@@ -147,4 +160,42 @@ impl APIWrapper {
         self.health().await?;
         Ok(time.elapsed())
     } 
+
+    /// Fetch a snapshot of metrics values from the prior minute along with refresh interval metadata. 
+    /// 
+    /// # Note
+    /// This function is intended to be polled once a minute and the values averaged to provide a clear and accurate
+    /// picture of the API's current load. As a result of its purpose, the relevant endpoint (and thus, this function)
+    /// is only accessible to staff members.
+    /// 
+    /// # Example
+    /// ```
+    /// let mut connections: HashMap<u64, u64> = HashMap::new();
+    /// let mut interval = tokio::time::interval(Duration::from_milis(1000));
+    /// 
+    /// loop {
+    ///     interval.tick().await;
+    /// 
+    ///     let metrics = wrapper.fetch_metrics().await?;
+    ///     connections.insert(metrics.interval().last(), metrics.get_metric("connections").unwrap());
+    /// }
+    /// ```
+    pub async fn fetch_metrics(&self) -> Result<MetricsSnapshot, APIError> {
+        self.get("/metrics").await
+    }
+
+    /// Fetch detailed information about a member.
+    /// 
+    /// # Note
+    /// The Member structure contains three Option fields. The values of these may be None if the member has chosen to
+    /// hide these fields from public view, or if they've disabled their account.
+    /// 
+    /// # Example
+    /// ```
+    /// let member = wrapper.fetch_member(87939).await?;
+    /// assert_eq!("Harry", member.username());
+    /// ```
+    pub async fn fetch_member(&self, member_id: u64) -> Result<Member, APIError> {
+        self.get(&format!("/members/{}", member_id)[..]).await
+    }
 }
